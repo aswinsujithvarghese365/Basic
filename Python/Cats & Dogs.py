4de409723567bf88deea49cfac37757ba4841fb6
Aswin Sujith Varghese <73499655+aswinsujithvarghese365@users.noreply.github.com>
2022-09-11 20:38:53 +0530

    Update Cats & Dogs.py

--- Python/Cats & Dogs.py
+++ Python/Cats & Dogs.py
@@ -1,9 +1,7 @@
 T = int(input(""))
 L = []
-c = []
-d = []
-c1 = 0
-d1 = 0
+c = 0
+d = 0
 for i  in range(0,T):
     vote = str(input(""))
     L.append(vote)
@@ -11,19 +9,15 @@ for j in range(0,T):
     vote_len = len(L[j])
     for k in range(0,vote_len):
         if(L[j][k] == "c"):
-            c1+=1
+            c+=1
         elif(L[j][k] == "d"):
-            d1+=1
-    c.append(c1)
-    d.append(d1)
-    c1=0
-    d1=0
-    
-for l in range(0,T):
-    c[l] = c[l] - d[l]
-    if(c[l]>d[l]):
+            d+=1
+    c-=d
+    if(c > d):
         print("cats")
-    elif(c[l] == d[l]):
+    elif(c == d):
         print("tie")
     else:
         print("dogs")
+    c = 0
+    d = 0
