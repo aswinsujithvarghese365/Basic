0311c8c8d2cfe45d1261fc564c4441743c60bc97
Aswin Sujith Varghese <73499655+aswinsujithvarghese365@users.noreply.github.com>
2023-03-18 15:32:23 +0530

    Create Non-Negative Product.py

---
+++ Codechef/Non-Negative Product.py
@@ -0,0 +1,12 @@
+"""Problem
+Alice has an array of N integers — 1,2,3.....,n. She wants the product of all the elements of the array to be a non-negative integer. That is, it can be either 
+0 or positive. But she doesn't want it to be negative. To do this, she is willing to remove some elements of the array. Determine the minimum number of elements that she will have to remove to make the product of the array's elements non-negative.
+
+Input Format
+The first line of input will contain a single integer T, denoting the number of test cases.
+The first line of each test case contains a single integer N — the number of elements in the array originally.
+The next line contains N space-separated integers — 1,2,3,....., n, which are the original array elements.
+ 
+Output Format
+For each test case, output on a new line the minimum number of elements that she has to remove from the array."""
+
